de1ae2bed918a11dc886c721100eba24517b1829
Luis Carlos <luiscarlos_ja@outlook.com>
2024-02-19 22:51:49 -0600

    Add bundle analyzer to next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 import path from "path";
 import { fileURLToPath } from "url";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 /** @type {import('next').NextConfig} */
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
+const bundleAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
 
@@ -25,4 +26,4 @@ const nextConfig = {
   },
 };
 
-export default withBundleAnalyzer(nextConfig);
+export default bundleAnalyzer(nextConfig);
